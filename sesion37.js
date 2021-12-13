deafbed03cb6ca5d121aca610b5c30a20b6fb933
taipicuri009 <priscilataipicuri009@gmail.com>
2021-12-13 18:53:37 -0500

    subiendo

---
+++ sesion37.js
@@ -0,0 +1,45 @@
+/*const listaDenumeros=[1,3,5,7,9,11]
+console.log(listaDenumeros[0])//1
+console.log(listaDenumeros[4])//9
+*/
+/*let numerosImpares =[1,3,5,7];
+cantidad = numerosImpares.length;
+console.log("primera forma:")
+for(i = 0; cantidad > i;i++){
+    console.log(numerosImpares[i])
+}*/
+/*let numerospares =[2,4,6,8,10,12];
+cantidad = numerospares.length;
+console.log("segunda forma:")
+for(i = 0; cantidad > i;i++){
+    console.log(numerospares[i])
+}*/
+//Reemplazar arreglos
+/*const listaDenumeros =[13,14,15,16,17,18]
+console.log(listaDenumeros[2])
+listaDenumeros[2] =16814564;
+console.log(listaDenumeros[2])*/
+//Agregar elementos a un arreglo
+/*listaDenumeros=[]
+listaDenumeros[0] =100;
+listaDenumeros[1] =102;
+listaDenumeros[2] =-10;
+listaDenumeros[3] =23;
+console.log(listaDenumeros[2])*/
+
+const mixedArray = [3, "hola mundo", 3627.2];
+cantidad = mixedArray.length;
+for(i = 0; cantidad > i ; i++){
+    console.log(mixedArray[i])
+}
+
+let questions = [
+    ['¿Cuál es la capital de Perú?','Lima'],
+    ['¿Cuántos millones de peruanos somos?','35.97'],
+    ['¿Cuál es la moneda de Perú?','Soles']
+]
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+console.log(questions[1][1]);
